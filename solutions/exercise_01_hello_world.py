55e191f71f9a3ff00e5bf21cfb6745396f65204f
fiddle-thomas <code.fiddle@gmail.com>
2024-07-02 22:52:23 +0530

    Add solution for exercise 01: Hello, World!

---
+++ solutions/exercise_01_hello_world.py
@@ -0,0 +1,6 @@
+# Prints 'Hello, world!' on the screen
+print("Hello, world!")
+# Asks the user for their name, takes the input and assigns it to the 'name' variable
+name = input("What is your name?\n")
+# Greets the user by their name
+print("Hello, " + name)
